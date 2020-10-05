3d78b0cd7e98bc25d123c1d04a77876c9f25a0e9
johnpiapian <johnpiapian@gmail.com>
2020-10-05 15:44:49 -0500

    Improve functionality

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -28,31 +28,113 @@ namespace LinkedList
                         linkedList.DisplayList();
                         break;
                     case 3: // add/remove at beginning of the list
+                        switch (getNumber("1) Add \n2) Remove \n3) Back \n"))
+                        {
+                            case 1:
 
+                                break;
+                            case 2:
+                                Console.WriteLine("Remove node at nth position.");
+                                break;
+                            case 3:
+                                break;
+                            default:
+                                Console.WriteLine("Invalid number!");
+                                break;
+                        }
                         break;
                     case 4: // add/remove at end of the list
 
                         break;
                     case 5: // add/remove nth position
+                        //int number = getNumber("Enter the position of the node you would like to remove: ");
+                        //Console.WriteLine("Your number is " + number);
+
+                        switch (getNumber("1) Add \n2) Remove \n3) Back \n"))
+                        {
+                            case 1:
+                                Console.WriteLine("Add node at nth position.");
+                                break;
+                            case 2:
+                                Console.WriteLine("Remove node at nth position.");
+                                break;
+                            case 3:
+                                break;
+                            default:
+                                Console.WriteLine("Invalid number!");
+                                break;
+                        }
 
                         break;
                     case 6: // reverse list
-                        linkedList.reserveList();
+                        try
+                        {
+                            linkedList.reserveList();
+                            Console.WriteLine("Successfully reversed list.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Unexpected error encountered:" + ex.Message);
+                        }
+
                         break;
                     case 7: // bubble sort data
-                        linkedList.bubbleSortExData();
+                        try
+                        {
+                            linkedList.bubbleSortExData();
+                            Console.WriteLine("Successfully sorted list by exchanging data.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Unexpected error encountered:" + ex.Message);
+                        }
                         break;
                     case 8: // bubble sort links
-                        linkedList.bubleSortExLinks();
+                        try
+                        {
+                            linkedList.bubleSortExLinks();
+                            Console.WriteLine("Successfully sorted list by exchange links.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Unexpected error encountered:" + ex.Message);
+                        }
                         break;
                     case 9: // insert cycle
-
+                        try
+                        {
+                            // ask for valuue
+                            linkedList.insertCycle(1);
+                            Console.WriteLine("Successfully inserted cycle.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Unexpected error encountered:" + ex.Message);
+                        }
                         break;
                     case 10: // detect cycle
-
+                        try
+                        {
+                            if (linkedList.hasCycle())
+                                Console.WriteLine("Cycle dected.");
+                            else
+                                Console.WriteLine("No cycle found.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Unexpected error encountered:" + ex.Message);
+                        }
                         break;
                     case 11: // remove cycle
-
+                        try
+                        {
+                            linkedList.removeCycle();
+                            Console.WriteLine("\nSuccessfully removed cycle.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Unexpected error encountered:" + ex.Message);
+                        }
                         break;
                     default:
                         Console.WriteLine("\nInvalid number!\n");
@@ -107,6 +189,22 @@ namespace LinkedList
             {
                 c = Console.ReadLine();
             } while (!Int32.TryParse(c, out result));
+
+            return result;
+        }
+
+        static int getNumber(string message)
+        {
+            Console.Write(message);
+
+            int result = 0;
+            string c = Console.ReadLine();
+
+            while(!Int32.TryParse(c, out result))
+            {
+                Console.Write("Check your input: ");
+                c = Console.ReadLine();
+            }
 
             return result;
         }
